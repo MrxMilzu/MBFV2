c5a6ce4d138f78fc5eb86ac960eb50c143cbc30a
MrxMilzu <89538890+MrxMilzu@users.noreply.github.com>
2021-08-26 12:15:45 +0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+clear
+echo
+echo "         _____  ____                "
+echo "        |  |  | |   \ \   /         "
+echo "        |  |  | |   /  \ /    MILZU "
+echo "  ______|_____|_|___\__/_\__●_____________________________ "
+echo
+pip install 
+echo
+echo "         _____  ____                "
+echo "        |  |  | |   \ \   /         "
+echo "        |  |  | |   /  \ /    MILZU "
+echo "  ______|_____|_|___\__/_\__●_____________________________ "
+echo
+pip install mechanize
+clear
+echo
+echo "         _____  ____                "
+echo "        |  |  | |   \ \   /         "
+echo "        |  |  | |   /  \ /    MILZU "
+echo "  ______|_____|_|___\__/_\__●_____________________________ "
+echo
+pip install requests bs4
+clear
+echo
+echo "         _____  ____                "
+echo "        |  |  | |   \ \   /         "
+echo "        |  |  | |   /  \ /    MILZU "
+echo "  ______|_____|_|___\__/_\__●_____________________________ "
+echo
+pip2 install requests
+clear
+echo
+echo "         _____  ____                "
+echo "        |  |  | |   \ \   /         "
+echo "        |  |  | |   /  \ /    MILZU "
+echo "  ______|_____|_|___\__/_\__●_____________________________ "
+echo
+pip2 install mechanize
+clear
+echo
+echo "         _____  ____                "
+echo "        |  |  | |   \ \   /         "
+echo "        |  |  | |   /  \ /    MILZU "
+echo "  ______|_____|_|___\__/_\__●_____________________________ "
+echo
+apt install wget -y
+clear
+echo
+echo "         _____  ____                "
+echo "        |  |  | |   \ \   /         "
+echo "        |  |  | |   /  \ /    MILZU "
+echo "  ______|_____|_|___\__/_\__●_____________________________ "
+echo
+apt install ruby 
+clear
+echo
+echo "         _____  ____                "
+echo "        |  |  | |   \ \   /         "
+echo "        |  |  | |   /  \ /    MILZU "
+echo "  ______|_____|_|___\__/_\__●_____________________________ "
+echo
+gem install lolcat
+clear
+echo
+echo "         ______       _         " | lolcat
+echo "        |_____  |     |   \ /   " | lolcat
+echo "        |       |____ |   / \   " | lolcat
+echo "  ______|_________________________________________________ " | lolcat
+echo "    FLIX            YOUTUBE MILZU-TC TOTORIAL77            " | lolcat
+echo
+clear
+echo
+echo
+echo "       [√] install commando done..."
+echo
+echo "     type python MBF.V2.py to continue "
+echo
