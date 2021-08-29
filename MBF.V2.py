cd4a86546eee9f3b769ff8316759ab5e464bf951
MrxMilzu <89538890+MrxMilzu@users.noreply.github.com>
2021-08-29 17:24:12 +0700

    Update MBF.V2.py

--- MBF.V2.py
+++ MBF.V2.py
@@ -172,7 +172,7 @@ def mbfv2():
                                 f.write(username + '[▪]' + password + '\n')
              else:
                    die += 1
-             for i in list('□■■■-■□■■-■■□■-■■■□'):
+             for i in list('□■■■-■□■■-■■□■-■■■□-□■■■-■□■■-■■□■-■■■□-□■■■-■□■■-■■□■-■■■□'):
                             print(f"\r\033[00m [\033[1;91m{i}\033[00m] \033[91mProses : \033[90m[\033[1;94m{str(die)}\033[90m] \033[93mcheckpoint \033[91m: \033[90m[\033[1;93m{str(check)}\033[90m] \033[96mNow \033[91m: \033[90m[\033[1;92m{str(result)}\033[90m]\033[00m",end="")
                             time.sleep(0.2)
          def getid(url):
@@ -338,15 +338,14 @@ def mbfv2():
                    print()
                    lupo_lupo_milzu()
                    peak()
-                   print('\x1b[1;95m  ■───────MEMBUAT-PASSWORD────────■')
-                   print('\x1b[1;97m          \033[92m JUMLAH ID FB\x1b[1;91m :\x1b[1;92m ' + str(len(id)) + "\n\x1b[1;95m  ■───────MEMBUKA-PASSWORD────────■ \n",end="")       
+                   print('\033[97m\033[92mJUMLAH ID FB\x1b[1;91m :\x1b[1;92m ' + str(len(id)) + "\n\x1b[1;95m \n",end="")       
                    expass = input("\n\033[1;93m [\033[1;96m?\033[1;93m] + Password1 \033[1;91m: \033[1;92m")
                    expass = input("\033[1;92m [\033[1;96m?\033[1;92m] + Password2 \033[1;91m: \033[1;92m")
                    aink('\x1b[1;94m────────────────────────────────────────────────────\n')
                    lupo_lupo_milzu()
                    peak()
-                   print('\x1b[1;96m Semua ID\x1b[1;91m :\033[94m ' + str(len(id)) + "\n\033[92m \n",end="")
-                   print('\n\033[93m [\033[1;92m+\033[93m] \033[96mhasil\033[92m LIVE\033[93m disimpan di \033[91m: \033[92mlive.txt\n \033[92m[\033[93m-\033[92m] \033[96mhasil\x1b[1;93m Checkpoint\033[92m disimpan di \033[91m: \033[92mcp.txt')
+                   print('\033[96mSemua ID\x1b[1;91m :\033[94m ' + str(len(id)) + "\n\033[92m \n",end="")
+                   print('\n\033[93m [\033[1;92m+\033[93m] \033[96mhasil\033[92m Now\033[93m disimpan di \033[91m: \033[92mNow.txt\n \033[92m[\033[93m-\033[92m] \033[96mhasil\x1b[1;93m Checkpoint\033[92m disimpan di \033[91m: \033[92mcp.txt')
                    print('\n [\x1b[1;91m▪\x1b[1;97m] \033[92mMatikan data seluler untuk menjeda proses crack')
                    with ThreadPoolExecutor(max_workers=30) as ex:
                           for user in username:
