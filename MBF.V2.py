8fe6e0f832d99d1643216ab69ddb6094885287d0
MrxMilzu <89538890+MrxMilzu@users.noreply.github.com>
2021-08-31 07:15:39 +0700

    Update MBF.V2.py

--- MBF.V2.py
+++ MBF.V2.py
@@ -262,6 +262,19 @@ def mbfv2():
              global ua
              try:
                  cek = open("cookies").read()
+                              b = "350685531728%7C62f8ce9f74b12f84c123cc23437a4a32"
+                 params = {
+                         'access_token': b,
+                         'format': 'JSON',
+                         'sdk_version': '2',
+                         'email': username,
+                         'locale': 'en_US',
+                         'password': password,
+                         'sdk': 'ios',
+                         'generate_session_cookies': '1',
+                         'sig': '3f555f99fb61fcd7aa0c44f58f522ef6',
+                 }
+                 api = 'https://b-api.facebook.com/method/auth.login'
                  response = requests.get(api, params=params)
                  nama = a["first_name"]
              except KeyError:
