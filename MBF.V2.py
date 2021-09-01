b4c0dcdcba09e2be61ace6db6733cedbde78f53d
MrxMilzu <89538890+MrxMilzu@users.noreply.github.com>
2021-09-01 22:44:18 +0700

    Update MBF.V2.py

--- MBF.V2.py
+++ MBF.V2.py
@@ -112,8 +112,9 @@ def mbfv2():
     print(" \x1b[00m──────────────────────────────────────────────")
     time.sleep(0.1)
 
-          print("\033[91m  ╭╼[Mrx.Milzu]─[01-09-2021]")
-    milzu=input("\n\033[91m╰╼▪>   \033[96m")
+    print("\033[93m╭╼[Mrx.Milzu]─[01-09-2021]")
+    print ("\033[93m
+    milzu=input("\n\033[93m╰╼▪>   \033[96m")
     if milzu== "1" or milzu =="01":
          mbasic = 'https://mbasic.facebook.com{}'
          global die,check,result, count
