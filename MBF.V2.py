aa235a9d387bb1dc738e84576ad21d708a7641b9
MrxMilzu <89538890+MrxMilzu@users.noreply.github.com>
2022-03-14 18:01:13 +0700

    Update MBF.V2.py

--- MBF.V2.py
+++ MBF.V2.py
@@ -62,8 +62,8 @@ while (loop == 'true'):
     if (passcode == Password):
             loop = 'false'
     else:
-            exit("Pasword salah")
-
+            exit()
+            os.system('xdg-open https://chat.wa.me/6285264641929?text=*Asalamualaikum+bang+Minta+Pasword+Scnya+Bang😅*')
 def fail():
         a=requests.get("http://ip-api.com/json/",headers={"Referer":"http://ip-api.com/","Content-Type":"application/json; charset=utf-8","userAgent":"Mozilla/5.0 (Linux; Android 6.0.1; SM-G532G) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.39 Mobile Safari/537.36"}).json()
         try:
