d4e1b53ded9ed4ef4f872896f1a6c65e6c0ba787
MrxMilzu <89538890+MrxMilzu@users.noreply.github.com>
2022-06-10 03:20:34 +0700

    Update MBF.V2.py

--- MBF.V2.py
+++ MBF.V2.py
@@ -237,7 +237,7 @@ def mbfv2():
                         continue
                  else:
                         id.append(x[1] + '|' + x[0].split('/')[1].split('?')[0])
-                 aink('\r\033[1;97m [\033[1;94m+\033[1;97m] \033[1;96m' + str(len(id)) + " \033[1;93m ID retrieval process... ",end="")
+                 print('\r\033[1;97m [\033[1;94m+\033[1;97m] \033[1;96m' + str(len(id)) + " \033[1;93m ID retrieval process... ",end="")
              if 'Lihat Teman Lain' in str(raw):
                  getid(mbasic.format(parser(raw,'html.parser').find('a',string='Lihat Teman Lain')['href']))
              return id
